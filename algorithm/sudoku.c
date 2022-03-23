5ca887cd8cfe066d7a07eb8a4cc226161e5b70e9
tik <78137991+wangduoyu666@users.noreply.github.com>
2022-03-23 18:40:07 +0800

    Update sudoku.c

--- algorithm/sudoku.c
+++ algorithm/sudoku.c
@@ -1,1 +1,37 @@
+/* 2022.2.23
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool line[9][1];
+bool column[1][9];
+bool dfs[9][9];
+bool block[3][3][9];
+int* space[81];
+int spacesize;
+
+void lico(char** cube, int dot) {
+	int i, j;
+	int* cu[9][1] = { 0 };
+	int* dfs[1][9] = { 0 };
+
+
+	for (i = 0; i < 9; i++) {
+		for (j = 0; j < 9; j++) {
+			if (line[i][1-1] != 0 && line[i++][1-1] = '.')
+				return true;
+			else
+			{
+				return false;
+			}
+			if (column[1-1][j] != 0 && cube[1-1][j++] = '.')
+				return true;
+			else {
+				return false;
+			}
+		}
+	}
+	return cube[i][j];
+}
+*/
 
